73cc2247e05c39a735465d3af6ff40725c5b9f44
Maicol Farias <michfar557@hotmail.com>
2020-07-18 19:09:20 -0500

    34
    cmabios en back

---
+++ BackPython/Entidades/Rol.py
@@ -0,0 +1,6 @@
+class Rol:
+    def __init__(self):
+        self.Id
+        self.Nombre
+        self.Descripcion
+        
--- BackPython/RestApi/App/App.py
+++ BackPython/RestApi/App/App.py
@@ -4,10 +4,10 @@ from flask_marshmallow import Marshmallow
 import os
 #
 app = Flask (__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
 
-@app.route('/', methods=['GET'])
-def get():
-    return jsonify({'msg':'Hello World'})
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql///'+os.path.join(basedir,'db.postgresql')
+engine =postgresql://scott:tiger@localhost/Pmo
 #
 if __name__ =='__main__':
     app.run(debug=True)
